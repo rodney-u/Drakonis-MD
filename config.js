939920e393f450d24772a9dfd8fbcce17e346bdc
rodney-u <rodneyraph4@gmail.com>
2025-07-26 16:23:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  SESSION_ID: "drakonis~IvNSmCyQ#yqP7F6a9qcPvJCbnhSVfxtx_wYxiNqWb2QzO2p1aFOI",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "254745314237", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
